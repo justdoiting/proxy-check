8783115e79aa19eb68ae856ac7eedcbad729a61a
justdoiting <109136320+justdoiting@users.noreply.github.com>
2026-06-04 15:51:53 +0800

    Update get.go

--- proxy/get.go
+++ proxy/get.go
@@ -489,7 +489,7 @@ func FetchSubsData(rawURL string) ([]byte, error) {
 	// UA 列表池
 	var uaList = []string{
 		convert.RandUserAgent(),
-		"mihomo/1.19.25",
+		"mihomo/1.19.26",
 		"clash.meta",
 		"curl/8.16.0",
 	}
